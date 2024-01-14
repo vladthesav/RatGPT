38d561e90de31d4790ee000a8c4c8545949bf4db
Vlad Kuz <47338837+vladthesav@users.noreply.github.com>
2024-01-14 11:23:42 -0500

    Create main.py with comments on what to do when I get home to my laptop

---
+++ main.py
@@ -0,0 +1,8 @@
+#ill implement this when i get home
+
+#should have 3 threads running
+
+## Listen - Record audio, detect speech, and send to ASR API (probs OpenAI Whisper)
+## Chat - Send transcribed and formated speech to some LLM (OpenAI GPT4)  
+## Speak - Send response to TTS API and play response
+
