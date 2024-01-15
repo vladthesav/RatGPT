c6377ef5db7804a69f1af8bb69cab24c0ed2b1a1
vladthesav <vladyslav.o.kuz@gmail.com>
2024-01-15 12:39:39 -0500

    Make OpenAI wrappers

--- src/Chat.py
+++ src/Chat.py
@@ -1,6 +1,45 @@
-#use some LLM to act as the brains
+from openai import OpenAI
 
-#hold convo history and format for LLM
-#then send to some OpenAI compatible API
 
-#the API shoukd be OpenAI compatible in case we want to use our own endpoint
+DEFAULT_SYSTEM_PROMPT = """
+You are RatGPT, a friendly rat sitting on your shoulder. 
+You will help the user with any task.
+You will keep it's responses short and to the point, as all outputs will be spoken via a TTS API."""
+
+
+class Chat:
+
+    def __init__(self, endpoint=None, model="gpt-3.5-turbo",system_prompt = DEFAULT_SYSTEM_PROMPT):
+
+        #set endpoint to something else if we want (e.g. huggingface, litellm, ollama, etc.)
+        if endpoint != None: pass 
+
+
+        self.client = OpenAI()
+        
+        self.model = model 
+        self.messages = [
+            {"role": "system", "content":system_prompt}
+        ]
+
+
+    def chat(self, content):
+        message ={"role": "user", "content": content}
+        self.messages.append(message)
+
+
+        completion = self.client.chat.completions.create(
+            model=self.model,
+            messages=self.messages
+            )
+
+        output = completion.choices[0].message 
+
+        return output
+
+
+
+
+#sanity check
+#chat = Chat() 
+#print(chat.chat("hi RatGPT, i want to make ratatouille"))
--- src/Speak.py
+++ src/Speak.py
@@ -1,3 +1,44 @@
-#this is how the rat talks to you
+from openai import OpenAI
 
-#this is just a wraper for some TTS API
+
+class Speak:
+
+    def __init__(self):
+        self.client = OpenAI()
+        self.model="tts-1"
+        self.voice="alloy"
+
+        #where should TTS output go?
+        self.destination = "file"
+
+
+    def tts(self, text):
+        response = self.client.audio.speech.create(
+            model=self.model,
+            voice=self.voice,
+            input=text
+            )
+
+        return response
+
+    def stream(self, tts_response, fpath = "out.mp3"):
+        #audio needs to go somewhere
+        dest = self.destination
+
+        if dest == 'file':
+            tts_response.stream_to_file(fpath)
+        else: 
+            pass
+
+    def speak(self, text):
+        tts_response = self.tts(text)
+        self.stream(tts_response)
+
+
+
+#im too lazy to write unit tests 
+#speak = Speak()
+#speak.speak("hi there, it's RatGPT")
+
+
+        
